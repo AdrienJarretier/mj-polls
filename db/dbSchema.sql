9d529a389c314304df0a8ff24a8bbefeb2bbc044
Adrien Jarretier-Yuste <jarretier.adrien@gmail.com>
2021-07-20 18:55:14 +0200

    foreign keys declaration back inside CREATE TABLE statements, alter table not supported in sqlite

--- db/dbSchema.sql
+++ db/dbSchema.sql
@@ -21,21 +21,18 @@ CREATE TABLE "duplicate_vote_check_methods" (
 CREATE TABLE "polls_choices" (
   "id" INTEGER PRIMARY KEY,
   "poll_id" INTEGER NOT NULL,
-  "name" VARCHAR(255) NOT NULL
+  "name" VARCHAR(255) NOT NULL,
+  FOREIGN KEY("poll_id") REFERENCES "polls" ("id")
 );
 
 CREATE TABLE "polls_votes" (
   "poll_choice_id" INTEGER NOT NULL,
   "grade_id" INTEGER NOT NULL,
-  "count" INTEGER DEFAULT 0
+  "count" INTEGER DEFAULT 0,
+  FOREIGN KEY("poll_choice_id") REFERENCES "polls_choices" ("id"),
+  FOREIGN KEY("grade_id") REFERENCES "grades" ("id")
 );
 
-ALTER TABLE "polls_choices" ADD FOREIGN KEY ("poll_id") REFERENCES "polls" ("id");
+CREATE UNIQUE INDEX unique_choice_per_poll ON "polls_choices" ("poll_id", "name");
 
-ALTER TABLE "polls_votes" ADD FOREIGN KEY ("poll_choice_id") REFERENCES "polls_choices" ("id");
-
-ALTER TABLE "polls_votes" ADD FOREIGN KEY ("grade_id") REFERENCES "grades" ("id");
-
-CREATE UNIQUE INDEX ON "polls_choices" ("poll_id", "name");
-
-CREATE UNIQUE INDEX ON "polls_votes" ("poll_choice_id", "grade_id");
+CREATE UNIQUE INDEX unique_vote_per_grade_per_choice ON "polls_votes" ("poll_choice_id", "grade_id");
