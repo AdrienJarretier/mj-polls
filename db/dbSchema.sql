1137af0034ab1a138c9f6c974033d6756aa35f21
Adrien Jarretier-Yuste <jarretier.adrien@gmail.com>
2021-07-20 13:21:55 +0200

    unique constraints on columns pairs on tables polls_choices and polls_votes

--- db/dbSchema.sql
+++ db/dbSchema.sql
@@ -1,31 +1,41 @@
-CREATE TABLE IF NOT EXISTS "grades" (
+CREATE TABLE "grades" (
   "id" INTEGER PRIMARY KEY,
-  "value" VARCHAR(255) NOT NULL UNIQUE,
-  "order" INTEGER NOT NULL UNIQUE
+  "value" VARCHAR(255) UNIQUE NOT NULL,
+  "order" INTEGER UNIQUE NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS "polls" (
+CREATE TABLE "polls" (
   "id" INTEGER PRIMARY KEY,
   "title" VARCHAR(255) NOT NULL,
   "max_voters" INTEGER,
   "max_datetime" DATETIME,
-  "datetime_opened" DATETIME DEFAULT CURRENT_TIMESTAMP,
+  "datetime_opened" DATETIME DEFAULT (CURRENT_TIMESTAMP),
   "datetime_closed" DATETIME
 );
 
-CREATE TABLE IF NOT EXISTS "polls_choices" (
+CREATE TABLE "duplicate_vote_check_methods" (
   "id" INTEGER PRIMARY KEY,
-  "poll_id" INTEGER NOT NULL,
-  "name" VARCHAR(255) NOT NULL,
-  FOREIGN KEY(poll_id) REFERENCES polls(id),
-  UNIQUE(poll_id,name)
+  "name" VARCHAR(255) UNIQUE NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS "polls_votes" (
+CREATE TABLE "polls_choices" (
+  "id" INTEGER PRIMARY KEY,
+  "poll_id" INTEGER NOT NULL,
+  "name" VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE "polls_votes" (
   "poll_choice_id" INTEGER NOT NULL,
   "grade_id" INTEGER NOT NULL,
-  "count" INTEGER DEFAULT 0,
-  FOREIGN KEY(poll_choice_id) REFERENCES polls_choices(id),
-  FOREIGN KEY(grade_id) REFERENCES grades(id),
-  UNIQUE(poll_choice_id,grade_id)
+  "count" INTEGER DEFAULT 0
 );
+
+ALTER TABLE "polls_choices" ADD FOREIGN KEY ("poll_id") REFERENCES "polls" ("id");
+
+ALTER TABLE "polls_votes" ADD FOREIGN KEY ("poll_choice_id") REFERENCES "polls_choices" ("id");
+
+ALTER TABLE "polls_votes" ADD FOREIGN KEY ("grade_id") REFERENCES "grades" ("id");
+
+CREATE UNIQUE INDEX ON "polls_choices" ("poll_id", "name");
+
+CREATE UNIQUE INDEX ON "polls_votes" ("poll_choice_id", "grade_id");
