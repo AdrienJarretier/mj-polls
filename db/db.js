df8de82b180093a47522cb49743f7d2927d65d88
Adrien Jarretier-Yuste <jarretier.adrien@gmail.com>
2021-07-27 22:40:18 +0200

    auto formatting

--- db/db.js
+++ db/db.js
@@ -2,83 +2,83 @@
 
 module.exports = function (opts) {
 
-let exports = {};
+    let exports = {};
 
-const dbUtils = require('./dbUtils.js')({
-    verbose: opts.verbose
-});
+    const dbUtils = require('./dbUtils.js')({
+        verbose: opts.verbose
+    });
 
-let executeStatement = dbUtils.executeStatement;
-let executeLoop = dbUtils.executeLoop;
+    let executeStatement = dbUtils.executeStatement;
+    let executeLoop = dbUtils.executeLoop;
 
-exports.getPollsIds = function () {
+    exports.getPollsIds = function () {
 
-    let rows = executeStatement(`
+        let rows = executeStatement(`
     SELECT id
     FROM polls;
     `,
-        'all')
+            'all')
 
-    let pollsIds = [];
+        let pollsIds = [];
 
-    for (let row of rows) {
-        pollsIds.push(row.id);
-    }
-
-    return pollsIds;
-
-}
-
-// for a list of results with polls and their choices
-// returns an object with polls ids as keys
-// values are the polls with an array of their choices
-function aggregateChoices(resultRows) {
-
-    let polls = {};
-
-    for (let row of resultRows) {
-
-        let id = row.polls.id;
-
-        if (!(id in polls)) {
-
-            polls[id] = {};
-
-            for (const [key, value] of Object.entries(row.polls)) {
-                polls[id][key] = value;
-            }
-
-            polls[id]['choices'] = [];
+        for (let row of rows) {
+            pollsIds.push(row.id);
         }
 
-        polls[id]['choices'].push(row.polls_choices);
+        return pollsIds;
 
     }
 
-    return polls;
+    // for a list of results with polls and their choices
+    // returns an object with polls ids as keys
+    // values are the polls with an array of their choices
+    function aggregateChoices(resultRows) {
 
-}
+        let polls = {};
+
+        for (let row of resultRows) {
+
+            let id = row.polls.id;
+
+            if (!(id in polls)) {
+
+                polls[id] = {};
+
+                for (const [key, value] of Object.entries(row.polls)) {
+                    polls[id][key] = value;
+                }
+
+                polls[id]['choices'] = [];
+            }
+
+            polls[id]['choices'].push(row.polls_choices);
+
+        }
+
+        return polls;
+
+    }
 
 
-exports.getFullPolls = function () {
+    exports.getFullPolls = function () {
 
-    let rows = executeStatement(`
+        let rows = executeStatement(`
     SELECT *
     FROM polls
     INNER JOIN polls_choices
     ON polls.id=polls_choices.poll_id;
     `,
-        'all', null, true);
+            'all', null, true);
 
-    let polls = aggregateChoices(rows);
+        let polls = aggregateChoices(rows);
 
-    return polls;
+        return polls;
 
-}
+    }
 
-exports.getMostRecentPolls = function (limit) {
+    exports.getMostRecentPolls = function (limit) {
 
-    let rows = executeStatement(`
+        let rows = executeStatement(`
 
     SELECT *
     FROM
@@ -89,278 +89,278 @@ exports.getMostRecentPolls = function (limit) {
     INNER JOIN polls_choices
     ON subq.id=polls_choices.poll_id;
     `,
-        'all', [limit], true);
+            'all', [limit], true);
 
-    let polls = aggregateChoices(rows);
+        let polls = aggregateChoices(rows);
 
-    return polls;
+        return polls;
 
-}
+    }
 
-exports.getPoll = function (id) {
+    exports.getPoll = function (id) {
 
-    let rows = executeStatement(`
+        let rows = executeStatement(`
     SELECT *
     FROM polls
     INNER JOIN polls_choices
     ON polls.id=polls_choices.poll_id
     WHERE polls.id = ?;
     `,
-        'all', [id], true);
+            'all', [id], true);
 
-    let poll = aggregateChoices(rows)[id];
+        let poll = aggregateChoices(rows)[id];
 
-    return poll;
+        return poll;
 
-}
-
-/*
-    data should be an object with at least those params :
-    {
-        title: string
-        maxVotes: null or int
-        max_datetime: null or string 'YYYY-MM-DD HH:MM:SS'
-        choices: array of strings : [choiceName1, ...]
-        duplicateCheckMethod: null or int (id of method)
     }
 
-    Returns the id of the inserted poll
-*/
-function insertPoll(data) {
+    /*
+        data should be an object with at least those params :
+        {
+            title: string
+            maxVotes: null or int
+            max_datetime: null or string 'YYYY-MM-DD HH:MM:SS'
+            choices: array of strings : [choiceName1, ...]
+            duplicateCheckMethod: null or int (id of method)
+        }
 
-    // ------------------------- prepare Data -------------------------
+        Returns the id of the inserted poll
+    */
+    function insertPoll(data) {
 
-    if (data.maxVotes == '')
-        data.maxVotes = null;
+        // ------------------------- prepare Data -------------------------
+
+        if (data.maxVotes == '')
+            data.maxVotes = null;
 
 
-    // ----------------------------------------------------------------
+        // ----------------------------------------------------------------
 
 
-    let pollsInsertResult;
+        let pollsInsertResult;
 
-    try {
+        try {
 
-        pollsInsertResult = executeStatement(`
+            pollsInsertResult = executeStatement(`
         INSERT INTO polls(title, max_voters, max_datetime)
         VALUES(?, ?, datetime(?));
         `, 'run', [data.title, data.maxVotes, data.max_datetime]);
 
-    }
-    catch (e) {
-        console.error('error inserting into polls');
-        console.error(e);
-    }
+        }
+        catch (e) {
+            console.error('error inserting into polls');
+            console.error(e);
+        }
 
-    let pollId = pollsInsertResult.lastInsertRowid;
+        let pollId = pollsInsertResult.lastInsertRowid;
 
-    // ------------------------ INSERT choices ------------------------
+        // ------------------------ INSERT choices ------------------------
 
-    let gradesIds = exports.getGrades().map(g => g.id);
+        let gradesIds = exports.getGrades().map(g => g.id);
 
-    let pcs_inserts_params = [];
+        let pcs_inserts_params = [];
 
-    // console.log('data.choices');
-    // console.log(data.choices);
+        // console.log('data.choices');
+        // console.log(data.choices);
 
-    for (let choiceName of data.choices) {
-        pcs_inserts_params.push([pollId, choiceName])
-    }
+        for (let choiceName of data.choices) {
+            pcs_inserts_params.push([pollId, choiceName])
+        }
 
-    // console.log('pcs_inserts_params', pcs_inserts_params);
+        // console.log('pcs_inserts_params', pcs_inserts_params);
 
-    let pcs_insertsResults
-    try {
-        pcs_insertsResults = executeLoop(`
+        let pcs_insertsResults
+        try {
+            pcs_insertsResults = executeLoop(`
         INSERT INTO polls_choices(poll_id, name)
         VALUES(?, ?);
         `, pcs_inserts_params);
-    }
-    catch (e) {
-        console.error('error inserting into polls_choices');
-        console.error(e);
-    }
-
-    // ----------------------------------------------------------------
-
-    // ---------------------- INSERT polls_votes ----------------------
-
-    let pvs_inserts_params = [];
-
-    for (let pc_insertResult of pcs_insertsResults) {
-
-        let pcId = pc_insertResult.lastInsertRowid;
-
-        for (let gradeId of gradesIds) {
-            pvs_inserts_params.push([pcId, gradeId]);
+        }
+        catch (e) {
+            console.error('error inserting into polls_choices');
+            console.error(e);
         }
 
-    }
+        // ----------------------------------------------------------------
 
-    // console.log('pvs_inserts_params', pvs_inserts_params);
+        // ---------------------- INSERT polls_votes ----------------------
 
-    try {
-        executeLoop(`
+        let pvs_inserts_params = [];
+
+        for (let pc_insertResult of pcs_insertsResults) {
+
+            let pcId = pc_insertResult.lastInsertRowid;
+
+            for (let gradeId of gradesIds) {
+                pvs_inserts_params.push([pcId, gradeId]);
+            }
+
+        }
+
+        // console.log('pvs_inserts_params', pvs_inserts_params);
+
+        try {
+            executeLoop(`
         INSERT INTO polls_votes(poll_choice_id, grade_id)
         VALUES(?, ?);
         `,
-            pvs_inserts_params
-        );
+                pvs_inserts_params
+            );
+        }
+        catch (e) {
+            console.error('error inserting into polls_votes');
+            console.error(e);
+        }
+
+        // ----------------------------------------------------------------
+
+        return pollsInsertResult.lastInsertRowid;
+
     }
-    catch (e) {
-        console.error('error inserting into polls_votes');
-        console.error(e);
+
+    function dummyInsertPoll(data) {
+
+        console.log(data);
+
     }
 
-    // ----------------------------------------------------------------
+    exports.insertPoll = insertPoll;
 
-    return pollsInsertResult.lastInsertRowid;
+    exports.addVote = function (vote) {
 
-}
+        console.log('adding vote');
 
-function dummyInsertPoll(data) {
+        let voteEntries = Object.entries(vote);
 
-    console.log(data);
-
-}
-
-exports.insertPoll = insertPoll;
-
-exports.addVote = function (vote) {
-
-    console.log('adding vote');
-
-    let voteEntries = Object.entries(vote);
-
-    let updatesResults = executeLoop(`
+        let updatesResults = executeLoop(`
     UPDATE polls_votes
     SET count = count+1
     WHERE poll_choice_id = ?
     AND grade_id = ?
     ;`,
-        voteEntries);
+            voteEntries);
 
 
-    // update unsuccessfull
-    if (updatesResults.length < voteEntries.length)
-        return false;
-
-    for (let updateResult of updatesResults) {
         // update unsuccessfull
-        if (updateResult.changes != 1)
+        if (updatesResults.length < voteEntries.length)
             return false;
+
+        for (let updateResult of updatesResults) {
+            // update unsuccessfull
+            if (updateResult.changes != 1)
+                return false;
+        }
+
+        // update success
+        return true;
+
     }
 
-    // update success
-    return true;
+    exports.getGrades = function () {
 
-}
-
-exports.getGrades = function () {
-
-    return executeStatement(`
+        return executeStatement(`
     SELECT * FROM grades ORDER BY "order";
     `
-        , 'all');
+            , 'all');
 
-}
+    }
 
 
 
-exports.getFullPoll = function (poll_id) {
+    exports.getFullPoll = function (poll_id) {
 
-    let poll = exports.getPoll(poll_id);
+        let poll = exports.getPoll(poll_id);
 
-    let polls_votes = executeStatement(`
+        let polls_votes = executeStatement(`
     SELECT pv.* FROM polls_votes AS pv
     INNER JOIN polls_choices AS pc ON pv.poll_choice_id=pc.id
     WHERE pc.poll_id = ?;
     `, 'all', [poll_id]);
 
-    let grades = exports.getGrades();
+        let grades = exports.getGrades();
 
-    for (let choice of poll.choices) {
+        for (let choice of poll.choices) {
 
-        choice['votes'] = {};
+            choice['votes'] = {};
 
-        for (let grade of grades) {
-            choice['votes'][grade.id] = Object.assign({}, grade);
-        }
-        // console.log(choice['votes']);
+            for (let grade of grades) {
+                choice['votes'][grade.id] = Object.assign({}, grade);
+            }
+            // console.log(choice['votes']);
 
-        for (let vote of polls_votes) {
+            for (let vote of polls_votes) {
 
-            if (vote.poll_choice_id == choice.id) {
+                if (vote.poll_choice_id == choice.id) {
 
-                // console.log(vote.grade_id, vote.count);
-                choice['votes'][vote.grade_id].count = vote.count;
-                // console.log(choice);
+                    // console.log(vote.grade_id, vote.count);
+                    choice['votes'][vote.grade_id].count = vote.count;
+                    // console.log(choice);
+
+                }
 
             }
 
         }
 
+        return poll;
+
     }
 
-    return poll;
+    // alternate Version, choices are the raw list from db inner join result
+    // exports.getVotes = function (poll_id) {
 
-}
+    //     return Object.assign(exports.getPoll(poll_id),
+    //         {
+    //             "choices": executeStatement(`
+    //             SELECT name, value, count, "order" FROM polls_votes AS pv
+    //             INNER JOIN polls_choices AS pc ON pv.poll_choice_id=pc.id
+    //             INNER JOIN polls on pc.poll_id=polls.id
+    //             INNER JOIN grades AS g on pv.grade_id=g.id
+    //             WHERE polls.id = ?
+    //             `, 'all', [poll_id], false)
+    //         });
 
-// alternate Version, choices are the raw list from db inner join result
-// exports.getVotes = function (poll_id) {
-
-//     return Object.assign(exports.getPoll(poll_id),
-//         {
-//             "choices": executeStatement(`
-//             SELECT name, value, count, "order" FROM polls_votes AS pv
-//             INNER JOIN polls_choices AS pc ON pv.poll_choice_id=pc.id
-//             INNER JOIN polls on pc.poll_id=polls.id
-//             INNER JOIN grades AS g on pv.grade_id=g.id
-//             WHERE polls.id = ?
-//             `, 'all', [poll_id], false)
-//         });
-
-// }
+    // }
 
 
-exports.getDuplicateCheckMethods = function () {
+    exports.getDuplicateCheckMethods = function () {
 
-    return executeStatement(`
+        return executeStatement(`
     SELECT * FROM duplicate_vote_check_methods`
-        , 'all');
+            , 'all');
 
-}
+    }
 
-/*
-    There are only 2 instances when a poll will close
-    1 - the number of votes exceed max_voters   =>  datetime_closed <- CURRENT_TIMESTAMP
-    2 - the max_datetime has expired            =>  datetime_closed <- max_datetime 
+    /*
+        There are only 2 instances when a poll will close
+        1 - the number of votes exceed max_voters   =>  datetime_closed <- CURRENT_TIMESTAMP
+        2 - the max_datetime has expired            =>  datetime_closed <- max_datetime 
 
-    reason : in, value of 1 or 2 as stated above.
-*/
-exports.closePoll = function (pollId, reason) {
+        reason : in, value of 1 or 2 as stated above.
+    */
+    exports.closePoll = function (pollId, reason) {
 
-    switch (reason) {
-        case 1:
+        switch (reason) {
+            case 1:
 
-            return executeStatement(`
+                return executeStatement(`
             UPDATE polls SET datetime_closed=CURRENT_TIMESTAMP WHERE id=?;
             `, 'run', [pollId]);
 
-        case 2:
+            case 2:
 
-            return executeStatement(`
+                return executeStatement(`
             UPDATE polls SET datetime_closed=max_datetime WHERE id=?;
             `, 'run', [pollId]);
 
-        default:
+            default:
 
-            throw 'arg : reason,  must be an integer with value in {1,2}';
+                throw 'arg : reason,  must be an integer with value in {1,2}';
+        }
+
     }
 
-}
-
-return exports;
+    return exports;
 
 };
 
--- db/dbUtils.js
+++ db/dbUtils.js
@@ -2,93 +2,93 @@
 
 module.exports = function (opts) {
 
-let exports = {};
+    let exports = {};
 
-const common = require("../common.js");
+    const common = require("../common.js");
 
-const config = common.serverConfig;
+    const config = common.serverConfig;
 
-const Database = require('better-sqlite3');
+    const Database = require('better-sqlite3');
 
-let verboseFun = null;
-if (opts.verbose)
-    verboseFun = console.log;
+    let verboseFun = null;
+    if (opts.verbose)
+        verboseFun = console.log;
 
-function _executePrepared(stmt, executionMethod, bindParameters, expand) {
+    function _executePrepared(stmt, executionMethod, bindParameters, expand) {
 
-    switch (executionMethod) {
+        switch (executionMethod) {
 
-        case 'all':
-            stmt.expand(expand);
-            return stmt.all(bindParameters);
+            case 'all':
+                stmt.expand(expand);
+                return stmt.all(bindParameters);
 
-        case 'get':
-            stmt.expand(expand);
-            return stmt.get(bindParameters);
+            case 'get':
+                stmt.expand(expand);
+                return stmt.get(bindParameters);
 
-        case 'run':
-            return stmt.run(bindParameters);
+            case 'run':
+                return stmt.run(bindParameters);
+
+        }
 
     }
 
-}
-
-function executeLoop(sqlString, arrayOfBindParameters) {
+    function executeLoop(sqlString, arrayOfBindParameters) {
 
 
-    // console.log('executeLoop');
+        // console.log('executeLoop');
 
-    const db = new Database(config.db.database);
+        const db = new Database(config.db.database);
 
-    // console.log('db opened');
-    let arrayOfResults = [];
+        // console.log('db opened');
+        let arrayOfResults = [];
 
-    const stmt = db.prepare(sqlString);
-    // console.log('stmt prepared');
+        const stmt = db.prepare(sqlString);
+        // console.log('stmt prepared');
 
-    const runMany = db.transaction((arrayOfBindParameters) => {
+        const runMany = db.transaction((arrayOfBindParameters) => {
 
-        // console.log('run many with :', arrayOfBindParameters)
+            // console.log('run many with :', arrayOfBindParameters)
 
-        for (let bindParameters of arrayOfBindParameters) {
-            bindParameters = bindParameters || [];
+            for (let bindParameters of arrayOfBindParameters) {
+                bindParameters = bindParameters || [];
 
-            // console.log('run with', bindParameters);
-            arrayOfResults.push(stmt.run(bindParameters));
-        }
+                // console.log('run with', bindParameters);
+                arrayOfResults.push(stmt.run(bindParameters));
+            }
 
-    });
+        });
 
-    // console.log('arrayOfBindParameters', arrayOfBindParameters);
-    runMany(arrayOfBindParameters);
-
-
-    db.close();
-
-    return arrayOfResults;
-
-}
-
-function executeStatement(sqlString, executionMethod, bindParameters, expand) {
-
-    bindParameters = bindParameters || [];
-    expand = expand || false;
-
-    const db = new Database(config.db.database, { verbose: verboseFun });
-    const stmt = db.prepare(sqlString);
-
-    let results = _executePrepared(stmt, executionMethod, bindParameters, expand);
-
-    db.close();
-
-    return results;
+        // console.log('arrayOfBindParameters', arrayOfBindParameters);
+        runMany(arrayOfBindParameters);
 
 
-}
+        db.close();
 
-exports.executeStatement = executeStatement;
-exports.executeLoop = executeLoop;
+        return arrayOfResults;
 
-return exports;
+    }
+
+    function executeStatement(sqlString, executionMethod, bindParameters, expand) {
+
+        bindParameters = bindParameters || [];
+        expand = expand || false;
+
+        const db = new Database(config.db.database, { verbose: verboseFun });
+        const stmt = db.prepare(sqlString);
+
+        let results = _executePrepared(stmt, executionMethod, bindParameters, expand);
+
+        db.close();
+
+        return results;
+
+
+    }
+
+    exports.executeStatement = executeStatement;
+    exports.executeLoop = executeLoop;
+
+    return exports;
 
 };
