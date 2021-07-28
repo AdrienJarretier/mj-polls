f886e8f93e48cfb2303231bc6ba2e00c03625255
Adrien Jarretier-Yuste <jarretier.adrien@gmail.com>
2021-07-28 16:05:13 +0200

    tests for already clçosed poll and valid reasons with open polls

--- tests/db.js
+++ tests/db.js
@@ -20,103 +20,102 @@ describe('db', function () {
 
   describe('#closePoll', function () {
 
-    let pollId;
-    before(function () {
+    describe('invalid reason', function () {
 
-      pollId = db.insertPoll({
-        title: 'testPoll',
-        maxVotes: null,
-        max_datetime: null,
-        choices: ['testChoice1'],
-        duplicateCheckMethod: null
+      let pollId;
+      before(function () {
+
+        pollId = db.insertPoll({
+          title: 'testPoll invalid reason',
+          maxVotes: null,
+          max_datetime: null,
+          choices: ['testChoice1'],
+          duplicateCheckMethod: null
+        });
+
+      });
+
+      it('should throw an error if reason is invalid', function () {
+        const possibleReasons = [1, 2];
+        assert.throws(db.closePoll, 'arg : reason,  must be an integer with value in ' + possibleReasons);
+      });
+
+      it('should throw an error if reason is 1 and max_voters is null', function () {
+        assert.throws(function () { db.closePoll(pollId, 1) }, 'Can\'t close poll, max_voters is NULL');
+      });
+
+      it('should throw an error if reason is 2 and max_datetime is null', function () {
+        assert.throws(function () { db.closePoll(pollId, 2) }, 'Can\'t close poll, max_datetime is NULL');
       });
 
     });
 
-    it('should throw an error if reason is invalid', function () {
-      const possibleReasons = [1, 2];
-      assert.throws(db.closePoll, 'arg : reason,  must be an integer with value in ' + possibleReasons);
+    describe('valid reasons and opened poll', function () {
+
+      let pollId;
+      beforeEach(function () {
+
+        pollId = db.insertPoll({
+          title: 'testPoll valid reasons and opened poll',
+          maxVotes: 1,
+          max_datetime: '2100-01-01 00:00:00',
+          choices: ['testChoice1'],
+          duplicateCheckMethod: null
+        });
+
+      });
+
+      it('if reason is 1 and max_voters is not null, should set datetime_closed to CURRENT_TIMESTAMP', function () {
+
+        let dateBefore = (new Date());
+        dateBefore.setMilliseconds(0);
+
+        db.closePoll(pollId, 1);
+        let dateAfter = (new Date());
+        let dateClosed = (new Date(db.getPoll(pollId).datetime_closed + 'Z'));
+
+        assert.isAtLeast(dateClosed, dateBefore);
+        assert.isAtMost(dateClosed, dateAfter);
+      });
+
+      it('if reason is 2 and max_datetime is not null, should set datetime_closed to max_datetime', function () {
+
+        db.closePoll(pollId, 2);
+
+        let poll = db.getPoll(pollId);
+        let dateClosed = (new Date(poll.datetime_closed + 'Z'));
+        let dateMax = (new Date(poll.max_datetime + 'Z'));
+
+        assert.deepEqual(dateClosed, dateMax);
+      });
+
+
     });
 
-    it('should throw an error if reason is 1 and max_voters is null', function () {
-      assert.throws(function () { db.closePoll(pollId, 1) }, 'Can\'t close poll, max_voters is NULL');
+    describe('valid reason but already closed poll', function () {
+
+      let pollId;
+      before(function () {
+
+        pollId = db.insertPoll({
+          title: 'testPoll valid reason but already closed poll',
+          maxVotes: 1,
+          max_datetime: '2100-01-01 00:00:00',
+          choices: ['testChoice1'],
+          duplicateCheckMethod: null
+        });
+
+        // closing poll to test later if error is thrown
+        db.closePoll(pollId, 1);
+
+      });
+
+      it('should throw an error if poll is already closed', function () {
+        assert.throws(function () { db.closePoll(pollId, 1) }, 'poll is already closed');
+        assert.throws(function () { db.closePoll(pollId, 2) }, 'poll is already closed');
+      });
+
     });
-
-    it('should throw an error if reason is 2 and max_datetime is null', function () {
-      assert.throws(function () { db.closePoll(pollId, 2) }, 'Can\'t close poll, max_datetime is NULL');
-    });
-
-    // describe('#closePoll(pollId, 1)', function () {
-
-    //   it('should set datetime_closed to CURRENT_TIMESTAMP', function () {
-
-    //     let dateBefore = (new Date());
-    //     dateBefore.setMilliseconds(0);
-
-    //     db.closePoll(pollId, 1);
-    //     let dateAfter = (new Date());
-    //     let dateClosed = (new Date(db.getPoll(pollId).datetime_closed + 'Z'));
-
-    //     assert.isAtLeast(dateClosed, dateBefore);
-    //     assert.isAtMost(dateClosed, dateAfter);
-    //   });
-
-    // });
-
-    // describe('#closePoll(pollId, 2)', function () {
-
-    //   it('should set datetime_closed to max_datetime', function () {
-
-    //     db.closePoll(pollId, 2);
-
-    //     let poll = db.getPoll(pollId);
-    //     let dateClosed = (new Date(poll.datetime_closed + 'Z'));
-    //     let dateMax = (new Date(poll.max_datetime + 'Z'));
-
-    //     assert.equal(dateClosed, dateMax);
-    //   });
-
-    //   });
-
-
-
-
-    //   describe('#closePoll(pollId, 1)', function () {
-
-    //     it('should set datetime_closed to CURRENT_TIMESTAMP', function () {
-
-    //       let dateBefore = (new Date());
-    //       dateBefore.setMilliseconds(0);
-
-    //       // console.log(dateBefore.toISOString());
-
-    //       db.closePoll(pollId, 1);
-    //       let dateAfter = (new Date());
-    //       let dateClosed = (new Date(db.getPoll(pollId).datetime_closed + 'Z'));
-
-    //       // console.log(dateClosed.toISOString());
-    //       // console.log(dateAfter.toISOString());
-
-    //       assert.isAtLeast(dateClosed, dateBefore);
-    //       assert.isAtMost(dateClosed, dateAfter);
-    //     });
-
-    //   });
-
-    //   describe('#closePoll(pollId, 2)', function () {
-
-    //     it('should set datetime_closed to max_datetime', function () {
-
-    //       db.closePoll(pollId, 2);
-
-    //       let poll = db.getPoll(pollId);
-    //       let dateClosed = (new Date(poll.datetime_closed + 'Z'));
-    //       let dateMax = (new Date(poll.max_datetime + 'Z'));
-
-    //       assert.equal(dateClosed, dateMax);
-    //     });
-
-    //   });
 
   });
 
