90d92f83afd875d2bcd7b7f7f01a282d19e18029
Adrien Jarretier-Yuste <jarretier.adrien@gmail.com>
2021-08-03 20:31:36 +0200

    more tests

--- public/tests/order_candidates.js
+++ public/tests/order_candidates.js
@@ -1,6 +1,67 @@
 var assert = chai.assert;
 describe('order_candidates()', function () {
 
+
+
+    it('should throw error if number of votes is different per candidates', function () {
+
+        let choices = [
+            {
+                "name": "last",
+                "votes": {
+                    "1": {
+                        "value": "Excellent",
+                        "order": 60,
+                        "count": 0,
+                    },
+                    "2": {
+                        "value": "Good",
+                        "order": 40,
+                        "count": 0,
+                    }
+                }
+            },
+            {
+                "name": "best",
+                "votes": {
+                    "1": {
+                        "value": "Excellent",
+                        "order": 60,
+                        "count": 9,
+                    },
+                    "2": {
+                        "value": "Good",
+                        "order": 40,
+                        "count": 0,
+                    }
+                }
+            },
+            {
+                "name": "middle",
+                "votes": {
+                    "1": {
+                        "value": "Excellent",
+                        "order": 60,
+                        "count": 5,
+                    },
+                    "2": {
+                        "value": "Good",
+                        "order": 40,
+                        "count": 4,
+                    }
+                }
+            }
+        ];
+
+        assert.throws(function () {
+            order_candidates(choices);
+        }, "the number of votes by candidate is not the same");
+
+    });
+
+
+
+
     describe('Only one candidate, one grade and 0 vote', function () {
 
         it('should return the only candidate', function () {
@@ -275,8 +336,96 @@ describe('order_candidates()', function () {
                         },
                         "4": {
                             "value": "Bad",
-                            "order": 9,
-                            "count": 0,
+                            "order": 0,
+                            "count": 9,
+                        }
+                    }
+                }
+            ];
+
+            assert.deepEqual(order_candidates(choices), ["best", "middle", "last"]);
+
+        });
+
+    });
+
+    describe('Testing time to sort out equalities', function () {
+
+        it('perfect equality, should return the candidates in the original order', function () {
+
+            let choices = [
+                {
+                    "name": "last",
+                    "votes": {
+                        "1": {
+                            "value": "Excellent",
+                            "order": 60,
+                            "count": 1000,
+                        },
+                        "2": {
+                            "value": "Good",
+                            "order": 40,
+                            "count": 1000,
+                        },
+                        "3": {
+                            "value": "Passble",
+                            "order": 20,
+                            "count": 1000,
+                        },
+                        "4": {
+                            "value": "Bad",
+                            "order": 0,
+                            "count": 1000,
+                        }
+                    }
+                },
+                {
+                    "name": "best",
+                    "votes": {
+                        "1": {
+                            "value": "Excellent",
+                            "order": 60,
+                            "count": 1000,
+                        },
+                        "2": {
+                            "value": "Good",
+                            "order": 40,
+                            "count": 1000,
+                        },
+                        "3": {
+                            "value": "Passble",
+                            "order": 20,
+                            "count": 1000,
+                        },
+                        "4": {
+                            "value": "Bad",
+                            "order": 0,
+                            "count": 1000,
+                        }
+                    }
+                },
+                {
+                    "name": "middle",
+                    "votes": {
+                        "1": {
+                            "value": "Excellent",
+                            "order": 60,
+                            "count": 1000,
+                        },
+                        "2": {
+                            "value": "Good",
+                            "order": 40,
+                            "count": 1000,
+                        },
+                        "3": {
+                            "value": "Passble",
+                            "order": 20,
+                            "count": 1000,
+                        },
+                        "4": {
+                            "value": "Bad",
+                            "order": 0,
+                            "count": 1000,
                         }
                     }
                 }
