8eb6fdce6a6ceaae91c2825cd2ae5b3a6799aa7e
Oceane Cassan <oceane.cssn@gmail.com>
2021-08-04 03:11:07 +0200

    fixed bug in which ties kept their after removal MM when compared to their non tie candidates later

--- public/javascripts/order_candidates.js
+++ public/javascripts/order_candidates.js
@@ -87,12 +87,22 @@ function compare(a, b) {
     return 0;
 }
 
+function compare_for_ties(a, b) {
+    if (a.majority_grade_for_ties_order < b.majority_grade_for_ties_order) {
+        return 1;
+    }
+    if (a.majority_grade_for_ties_order > b.majority_grade_for_ties_order) {
+        return -1;
+    }
+    return 0;
+}
+
 function get_majority_grades(choices, majority, for_ties) {
 
     for_ties = for_ties || false;
 
 
-    // funtion that addes to choices the majority grade of each candidate, and its order
+    // funtion that adds to choices the majority grade of each candidate, and its order
     for (const choice of choices) {
 
         var votes = choice.votes;
@@ -102,6 +112,7 @@ function get_majority_grades(choices, majority, for_ties) {
         for (const vote of Object.keys(votes).reverse()) {
 
             if (for_ties) {
+                console.log("passing in for ties for candidate " + choice.name)
                 cpt += votes[vote].count_for_ties;
 
             }
@@ -109,43 +120,67 @@ function get_majority_grades(choices, majority, for_ties) {
                 cpt += votes[vote].count;
                 // adding new count field to be used later for removing votes and discriminate ties
                 votes[vote].count_for_ties = votes[vote].count;
+                // console.log("adding " + votes[vote].count + " votes on grade " + votes[vote].value + " and candidate " + choice.name);
             }
 
 
             if (cpt >= majority & !majority_found) {
 
-                choice["majority_grade"] = votes[vote].value;
-                choice["majority_grade_order"] = votes[vote].order;
+                if (for_ties) {
+                    choice["majority_grade_for_ties"] = votes[vote].value;
+                    choice["majority_grade_for_ties_order"] = votes[vote].order;
+                    console.log("In ties : majority grade is " + votes[vote].value + " for candidate " + choice.name);
+                }
+                else {
+                    choice["majority_grade"] = votes[vote].value;
+                    choice["majority_grade_order"] = votes[vote].order;
+                    console.log("majority grade is " + votes[vote].value + " for candidate " + choice.name);
+                }
                 majority_found = true;
 
             }
         }
     }
     //reordering choices according to their majority grades
-    choices.sort(compare);
+
+    if (for_ties)
+        choices.sort(compare_for_ties);
+    else
+        choices.sort(compare);
 }
 
 function return_winner(choices, majority, for_ties) {
 
     for_ties = for_ties || false;
 
-    get_majority_grades(choices, majority, for_ties)
 
-    const winning_grade = choices[0].majority_grade;
+    get_majority_grades(choices, majority, for_ties);
 
-    const ties = choices.filter(function (el) {
-        return el.majority_grade == winning_grade;
+
+    var winning_grade;
+    var ties;
+
+    if (for_ties) {
+        winning_grade = choices[0].majority_grade_for_ties;
+        ties = choices.filter(function (el) {
+            return el.majority_grade_for_ties == winning_grade;
+        }
+        )
     }
-    )
+    else {
+        winning_grade = choices[0].majority_grade;
+        ties = choices.filter(function (el) {
+            return el.majority_grade == winning_grade;
+        }
+        )
+    }
 
     if (ties.length == 1) {
-        console.log("No ties, the winner is : " + choices[0].name);
-
-        // console.log(choices);
+        // console.log("No ties, the winner is : " + choices[0].name);
         return choices[0].name;
     }
     else {
-        console.log("There are " + ties.length + " ties");
+        // console.log("There are " + ties.length + " ties");
 
         // removes one vote for each winning grade in the tied candidates
 
@@ -170,7 +205,7 @@ function return_winner(choices, majority, for_ties) {
 
         // recursive call, that will continue until there are different majority grades between ties
 
-        console.log("Removed counts once to handle ties")
+        // console.log("Removed counts once to handle ties")
 
         return return_winner(ties, majority, true);
 
@@ -182,12 +217,19 @@ function order_candidates_(choices, majority) {
     if (choices.length == 1)
         return [choices[0].name];
 
+    const VOTERS_COUNT = get_voters_count(choices);
+
+    if (VOTERS_COUNT == 0)
+        return [choices[0].name];
+
 
 
     let winner = return_winner(choices, majority);
     let the_rest = choices.filter(function (el) {
         return el.name != winner;
     })
+
+    // console.log("Winner is now " + winner);
 
     return [winner].concat(order_candidates_(the_rest));
 
