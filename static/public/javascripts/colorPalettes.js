19c9257411502ca4313a9fa9a2ddf466f994fc9c
Adrien Jarretier-Yuste <jarretier.adrien@gmail.com>
2021-08-18 13:02:09 +0200

    remove dunused color palettes

--- static/public/javascripts/colorPalettes.js
+++ static/public/javascripts/colorPalettes.js
@@ -1,20 +1,5 @@
 'use strict';
 
-// const COLORS_7 = [
-//     '#00935e', '#50b67f', '#9fd99f', '#effcc0', '#FBD989', '#e29e6d', '#c96251'
-// ].reverse();
-
-
-// const COLORS_3 = [
-//     "#df8568", "#f8e6b5", "#54a062"
-// ].reverse();
-
-// const COLORS_5 = [
-//     "#df8568", "#feb39a", "#f8e6b5", "#7ebc89", "#54a062"
-// ].reverse();
-
-
-
 export default {
 
     COLORS_7: [
