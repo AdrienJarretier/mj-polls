cea67f4717ae702ee9eecea7a36ff1e6b3bb0009
Adrien Jarretier-Yuste <jarretier.adrien@gmail.com>
2021-08-20 23:20:03 +0200

    renamed file

--- routes/index.js
+++ routes/index.js
@@ -104,8 +104,8 @@ router.get('/createPoll', handleCreatePoll('createPoll'));
 router.get('/poll/:id', handlePollView('poll'), renderPollResults);
 
 
-router.get('/newCreate', handleCreatePoll('displayOrCreatePoll'), renderPollResults);
-router.get('/newPoll/:id', handlePollView('displayOrCreatePoll'), renderPollResults);
+router.get('/newCreate', handleCreatePoll('poll_display_create'), renderPollResults);
+router.get('/newPoll/:id', handlePollView('poll_display_create'), renderPollResults);
 
 
 router.get('/poll_results/:id', function (req, res, next) {
