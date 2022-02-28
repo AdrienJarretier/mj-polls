8505a75edbc8f3b8e7df2af1db2c68ba3ed790ab
Adrien Jarretier-Yuste <jarretier.adrien@gmail.com>
2022-02-28 14:50:36 +0100

    independant polls insertion for each test of ClosePoll

--- v2/tests/DbClosePollTest.php
+++ v2/tests/DbClosePollTest.php
@@ -14,35 +14,48 @@ final class DbClosePollTest extends TestCase
         // echo "set up\n";
         self::$db = new Db('mjpolls_unittests');
     }
-    protected function setUp(): void
+
+    function testClosePollFailsIfReasonInvalid()
     {
-        $this->pollId = self::$db->insertPoll(
+        $pollId = self::$db->insertPoll(
             new Poll([
                 'title' => 'testPoll close with invalid reason'
             ]),
             ['testChoice1']
         );
-    }
-    function testClosePollFailsIfReasonInvalid()
-    {
+
         $possibleReasons = [1, 2];
         $this->expectExceptionMessage('arg : reason,  must be an integer with value in ' . implode(',', $possibleReasons));
-        self::$db->closePoll($this->pollId, 3);
+        self::$db->closePoll($pollId, 3);
     }
     /**
      * @testdox should throw an error if reason is 1 and max_voters is null
      */
     function testClosePollFailsIfMaxVotersNull()
     {
+        $pollId = self::$db->insertPoll(
+            new Poll([
+                'title' => 'testPoll close with invalid reason'
+            ]),
+            ['testChoice1']
+        );
+
         $this->expectExceptionMessage('Can\'t close poll, max_voters is NULL');
-        self::$db->closePoll($this->pollId, 1);
+        self::$db->closePoll($pollId, 1);
     }
     /**
      * @testdox should throw an error if reason is 2 and max_datetime is null
      */
     function testClosePollFailsIfMaxDatetimeNull()
     {
+        $pollId = self::$db->insertPoll(
+            new Poll([
+                'title' => 'testPoll close with invalid reason'
+            ]),
+            ['testChoice1']
+        );
+
         $this->expectExceptionMessage('Can\'t close poll, max_datetime is NULL');
-        self::$db->closePoll($this->pollId, 2);
+        self::$db->closePoll($pollId, 2);
     }
 }
