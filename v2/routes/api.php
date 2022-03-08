2c17985e209c9cc354c95b8569e71c53ef3663b7
Adrien Jarretier-Yuste <jarretier.adrien@gmail.com>
2022-03-08 17:57:05 +0100

    removed debug error display

--- v2/routes/api.php
+++ v2/routes/api.php
@@ -1,10 +1,4 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-?>
-
-<?php
 
 require_once __DIR__ . '/../db/Db.php';
 require_once __DIR__ . '/RoutesCommon.php';
--- v2/views/404.php
+++ v2/views/404.php
@@ -1,9 +1,3 @@
-<?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 
