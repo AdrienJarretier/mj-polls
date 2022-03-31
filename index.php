55ba6f4c520c68633b5671bb56fb356bfd441789
Adrien Jarretier-Yuste <jarretier.adrien@gmail.com>
2022-03-31 04:37:43 +0200

    moved locales route to dedicated router script

--- index.php
+++ index.php
@@ -25,34 +25,9 @@ Route::pathNotFound(function ($path) {
   include('views/404.php');
 });
 
-
-/**
- * 
- * @param string $part the part of the app to load messages for (or the page)
- * e.g : "header", "home"
- * @param string $locale e.g "fr-FR"
- * @return array array containing messages
- */
-Route::add('/locales/([a-z]+(?:-[a-z]+)*)/([a-z]{2}-[A-Z]{2})', function ($part, $locale) {
-
-  // echo 'requested : ' . $part . ' - ' . $locale;
-
-  $parts = explode('-', $part);
-
-  // Common::dlog(Common::$localesMsgs);
-  $localeMsgs = Common::$localesMsgs[$locale];
-
-  foreach ($parts as $part) {
-    $localeMsgs = $localeMsgs[$part];
-  }
-
-  // Common::log($localeMsgs);
-
-  echo json_encode($localeMsgs);
-});
-
-Router::use('/', 'routes/index.php');;
-Router::use('/polls', 'routes/api.php');;
+Router::use('/', 'routes/index.php');
+Router::use('/polls', 'routes/api.php');
+Router::use('/locales', 'routes/locales.php');
 
 // Run the router
 Route::run('/');
