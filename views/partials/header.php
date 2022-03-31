4019ebeec431ef28ca60afcb39037d3762113436
Adrien Jarretier-Yuste <jarretier.adrien@gmail.com>
2022-04-01 00:19:28 +0200

    header using 2 spaces for tabs

--- views/partials/header.php
+++ views/partials/header.php
@@ -1,51 +1,51 @@
 <body>
 
-    <script type="module">
-        import {
-            LocaleMessages
-        } from '/javascripts/LocaleMessages.js';
+  <script type="module">
+    import {
+      LocaleMessages
+    } from '/javascripts/LocaleMessages.js';
 
-        const localeMsgs = await LocaleMessages.new(
-            'client-header');
+    const localeMsgs = await LocaleMessages.new(
+      'client-header');
 
-        // console.log(localeMsgs);
+    // console.log(localeMsgs);
 
-        $(function() {
+    $(function() {
 
-            let nav = $('nav a button')
-            const navMsgs = localeMsgs.get('nav');
-            for (let i = 0; i < nav.length; ++i) {
+      let nav = $('nav a button')
+      const navMsgs = localeMsgs.get('nav');
+      for (let i = 0; i < nav.length; ++i) {
 
-                let navButton = nav.eq(i);
-                const navButtonId = navButton.attr('id');
-                navButton.text(navMsgs.get(navButtonId));
-            }
+        let navButton = nav.eq(i);
+        const navButtonId = navButton.attr('id');
+        navButton.text(navMsgs.get(navButtonId));
+      }
 
-        });
-    </script>
+    });
+  </script>
 
-    <div class="container-md">
+  <div class="container-md">
 
-        <!-- Body and container div will be closed in footer -->
+    <!-- Body and container div will be closed in footer -->
 
-        <div class="row mb-4">
-            <div class="col">
-                <header>
+    <div class="row mb-4">
+      <div class="col">
+        <header>
 
-                    <img src="/images/logo.png" class="img-fluid" alt="Majority judgement banner" />
-                    <!-- <p class="fs-4">A web application for creating, running and visualizing majority judgment polls.</p> -->
+          <img src="/images/logo.png" class="img-fluid" alt="Majority judgement banner" />
+          <!-- <p class="fs-4">A web application for creating, running and visualizing majority judgment polls.</p> -->
 
-                    <nav class="navbar bg-dark bg-gradient shadow-lg p-2 px-3 rounded">
+          <nav class="navbar bg-dark bg-gradient shadow-lg p-2 px-3 rounded">
 
-                        <!-- <a href="/"><button class="btn btn-md btn-secondary" type="button" id="Home">Home</button></a> -->
+            <!-- <a href="/"><button class="btn btn-md btn-secondary" type="button" id="Home">Home</button></a> -->
 
-                        <a href="/createPoll"><button class="btn btn-md btn-secondary" type="button" id="Create-Poll">Create a
-                                poll</button></a>
+            <a href="/createPoll"><button class="btn btn-md btn-secondary" type="button" id="Create-Poll">Create a
+                poll</button></a>
 
-                        <a href="/context"><button class="btn btn-md btn-secondary" type="button" id="Context">Context</button></a>
+            <a href="/context"><button class="btn btn-md btn-secondary" type="button" id="Context">Context</button></a>
 
-                    </nav>
+          </nav>
 
-                </header>
-            </div>
-        </div>
+        </header>
+      </div>
+    </div>
