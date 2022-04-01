df1162317a4d6594f6603bf05c62936137b20a54
Adrien Jarretier-Yuste <jarretier.adrien@gmail.com>
2022-04-01 08:48:49 +0200

    navbar with toggler

--- views/partials/header.php
+++ views/partials/header.php
@@ -85,27 +85,40 @@
           <img src="/images/logo.png" class="img-fluid" alt="Majority judgement banner" />
           <!-- <p class="fs-4">A web application for creating, running and visualizing majority judgment polls.</p> -->
 
-          <nav class="navbar bg-dark bg-gradient shadow-lg p-2 px-3 rounded">
 
-            <!-- <a href="/"><button class="btn btn-md btn-secondary" type="button" id="Home">Home</button></a> -->
 
-            <a href="/createPoll"><button class="btn btn-md btn-secondary" type="button" id="Create-Poll">Create a
-                poll</button></a>
+          <nav class="navbar navbar-expand-md navbar-dark bg-dark bg-gradient shadow-lg p-2 px-3 rounded">
 
-            <span>
+            <div class="container-fluid">
 
-              <a href="/context"><button class="btn btn-md btn-secondary me-3" type="button" id="Context">Context</button></a>
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
 
-              <div class="dropdown d-inline">
-                <button class="btn btn-secondary dropdown-toggle" type="button" id="navLangDropdownLabel" data-bs-toggle="dropdown" aria-expanded="false">
-                </button>
-                <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="navLangDropdownLabel" id="navLangDropdownList">
-                </ul>
+              <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+
+                <a href="/createPoll"><button class="btn btn-md btn-secondary" type="button" id="Create-Poll">Create a
+                    poll</button></a>
+
+
+                <span>
+                  <a href="/context"><button class="btn btn-md btn-secondary me-3" type="button" id="Context">Context</button></a>
+
+                  <div class="dropdown d-inline">
+                    <button class="btn btn-secondary dropdown-toggle" type="button" id="navLangDropdownLabel" data-bs-toggle="dropdown" aria-expanded="false">
+                    </button>
+                    <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="navLangDropdownLabel" id="navLangDropdownList">
+                    </ul>
+                  </div>
+                </span>
+
+
               </div>
-
-            </span>
+            </div>
 
           </nav>
+
+
 
         </header>
       </div>
