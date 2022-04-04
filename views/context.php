866971fd609b6e4d789be7198638ecf57f8c6f62
Adrien Jarretier-Yuste <jarretier.adrien@gmail.com>
2022-04-04 03:50:01 +0200

    gets readme.md content, parse it and add it into context page while removing h1 title

--- views/context.php
+++ views/context.php
@@ -4,15 +4,39 @@
 <head>
   <?php include('partials/head.php') ?>
 
+  <script src="/extLibs/marked-4.0.12/marked.min.js"></script>
+
+  <script>
+    $(function() {
+
+      const readmeContent = <?= json_encode(file_get_contents(__DIR__ . '/../readme.md'), JSON_HEX_APOS); ?>;
+
+      let htmledReadme = $(marked.parse(readmeContent));
+
+      $('#content').html(htmledReadme).find('h1').remove();
+
+    });
+  </script>
+
 </head>
 
 <!-- Includes Body and container div opening tags as well as <header> -->
 <?php include('partials/header.php') ?>
 
+<div class="row">
+  <div id="content" class="col">
+
+  </div>
+</div>
+
+<?php
+
+// file_get_contents(__DIR__ . '/../readme.md');
+
+?>
 
 
-
-  <!-- Includes Body and container closing tags as well as <footer> -->
-      <?php include('partials/footer.php') ?>
+<!-- Includes Body and container closing tags as well as <footer> -->
+<?php include('partials/footer.php') ?>
 
 </html>
