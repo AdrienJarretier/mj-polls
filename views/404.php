b753c772c2dd67aee99804b6c0069a080576dcf0
Adrien Jarretier-Yuste <jarretier.adrien@gmail.com>
2022-04-19 21:17:06 +0200

    moved doctype, html and head tags into head.php

--- views/404.php
+++ views/404.php
@@ -1,7 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
     <?php include 'partials/head.php' ?>
 </head>
 
--- views/context.php
+++ views/context.php
@@ -1,7 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
   <?php include('partials/head.php') ?>
 
   <script src="/extLibs/marked-4.0.12/marked.min.js"></script>
